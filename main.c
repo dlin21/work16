47a9a2792a75679aeca2898c618837785f90459a
dlin21 <90690772+dlin21@users.noreply.github.com>
2022-01-27 22:24:55 -0500

    Update main.c

--- main.c
+++ main.c
@@ -7,28 +7,32 @@
 #include <errno.h>
 
 int main(){
-
+  printf("parent forking...");
   srand(time(NULL));
-
-  printf("Forking...\n");
+  
   int f1 = fork();
   int f2;
 
-  if(f1) f2 = fork();
+  if(f1) {
+    rand();
+    f2 = fork();
+  }
+  
   if(f1 && f2){
     int stat;
     int pid = wait(&stat);
 
-    printf("child pid: %d\t asleep for %d second\n", stat, pid);
+    printf("parent: child pid: %d\t asleep for %d second\n", pid, WEXITSTATUS(stat));
     printf("parent process complete\n");
+    return 0;
   }
 
   else{
-    printf("child pid:%d\n ", getpid());
+    printf("child pid: %d\n ", getpid());
     int i = rand() % 4 + 2; // gets a number from 2 to 5
     sleep(i);
-    printf("child %d done\n", getpid());
-    return i;
+    printf("child slept for %d seconds\n", i);
+    return i
   }
 
   return 0;
